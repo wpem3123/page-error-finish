ce8d455df67f5b47b65638f6e5ffda37b63dd2cb
wpem3123 <wpem3123@naver.com>
2022-09-08 15:29:24 +0900

    검색기능까지 완료

--- src/main/java/site/metacoding/red/web/BoardsController.java
+++ src/main/java/site/metacoding/red/web/BoardsController.java
@@ -9,12 +9,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.red.domain.boards.Boards;
 import site.metacoding.red.domain.boards.BoardsDao;
 import site.metacoding.red.domain.users.Users;
+import site.metacoding.red.web.dto.request.boards.UpdateDto;
 import site.metacoding.red.web.dto.request.boards.WriteDto;
 import site.metacoding.red.web.dto.response.boards.MainDto;
 import site.metacoding.red.web.dto.response.boards.PagingDto;
@@ -27,29 +27,87 @@ public class BoardsController {
 	private final BoardsDao boardsDao;
 	// @PostMapping("/boards/{id}/delete")
 	// @PostMapping("/boards/{id}/update")
-	
-	@PostMapping("/boards/{id}/delete")
-	public String deleteBoards(@PathVariable Integer id) {
-		Users principal = (Users) session.getAttribute("principal");
+
+	@PostMapping("/boards/{id}/update")
+	public String update(@PathVariable Integer id, UpdateDto updateDto) {
+		// 1. 영속화
 		Boards boardsPS = boardsDao.findById(id);
-		
+		Users principal = (Users) session.getAttribute("principal");
 		// 비정상 요청 체크
-		if(boardsPS == null) { // if는 비정상 로직을 타게 해서 걸러내는 필터 역할을 하는게 좋다.
-			return "redirect:/boards/"+id;
+		if (boardsPS == null) {
+			return "errors/badPage";
 		}
-		
 		// 인증 체크
 		if (principal == null) {
 			return "redirect:/loginForm";
 		}
-		
 		// 권한 체크 ( 세션 principal.getId() 와 boardsPS의 userId를 비교)
-		if(principal.getId() != boardsPS.getUsersId()) {
-			return "redirect:/boards/"+id;
+		if (principal.getId() != boardsPS.getUsersId()) {
+			return "errors/badPage";
 		}
-		
-			
-		boardsDao.delete(id);
+
+		// 2. 변경
+		boardsPS.글수정(updateDto);
+
+		// 3. 수행
+		boardsDao.update(boardsPS);
+
+		return "redirect:/boards/" + id;
+	}
+
+	@GetMapping("/boards/{id}/updateForm")
+	public String updateForm(@PathVariable Integer id, Model model) {
+		Boards boardsPS = boardsDao.findById(id);
+		Users principal = (Users) session.getAttribute("principal");
+
+		// 비정상 요청 체크
+		if (boardsPS == null) {
+			return "errors/badPage";
+		}
+		// 인증 체크
+		if (principal == null) {
+			return "redirect:/loginForm";
+		}
+		// 권한 체크 ( 세션 principal.getId() 와 boardsPS의 userId를 비교)
+		if (principal.getId() != boardsPS.getUsersId()) {
+			return "errors/badPage";
+		}
+
+		model.addAttribute("boards", boardsPS);
+
+		return "boards/updateForm";
+	}
+
+	@PostMapping("/boards/{id}/delete")
+	public String deleteBoards(@PathVariable Integer id) {
+		Users principal = (Users) session.getAttribute("principal");
+		Boards boardsPS = boardsDao.findById(id);
+
+		// 비정상 요청 체크
+		if (boardsPS == null) { // if는 비정상 로직을 타게 해서 걸러내는 필터 역할을 하는게 좋다.
+			System.out.println("=================");
+			System.out.println("없는 번호를 요청하였습니다");
+			System.out.println("=================");
+			return "errors/badPage";
+		}
+
+		// 인증 체크
+		if (principal == null) {
+			System.out.println("=================");
+			System.out.println("로그인을 안하셨어요");
+			System.out.println("=================");
+			return "redirect:/loginForm";
+		}
+
+		// 권한 체크 ( 세션 principal.getId() 와 boardsPS의 userId를 비교)
+		if (principal.getId() != boardsPS.getUsersId()) {
+			System.out.println("=================");
+			System.out.println("해당 글을 삭제할 권한이 없습니다");
+			System.out.println("=================");
+			return "redirect:/boards/" + id;
+		}
+
+		boardsDao.delete(id); // 핵심 로직
 		return "redirect:/";
 	}
 
@@ -71,20 +129,33 @@ public class BoardsController {
 		return "redirect:/";
 	}
 
+	// 1번째 ?page=0&keyword=스프링
 	@GetMapping({ "/", "/boards" })
-	public String getBoardList(Model model, Integer page) { // 0 -> 0, 1->10, 2->20
-		if (page == null)
+	public String getBoardList(Model model, Integer page, String keyword) { // 0 -> 0, 1->10, 2->20
+		if (page == null) {
 			page = 0;
+		}
 		int startNum = page * 3;
+			
+		if (keyword == null || keyword.isEmpty()) {
+			List<MainDto> boardsList = boardsDao.findAll(startNum);
+			PagingDto paging = boardsDao.paging(page, null);
+			paging.makeBlockInfo(keyword);
 
-		List<MainDto> boardsList = boardsDao.findAll(startNum);
-		PagingDto paging = boardsDao.paging(page);
-		paging.makeBlockInfo();
-		
-		model.addAttribute("boardsList", boardsList);
-		model.addAttribute("paging", paging);
-		
+			model.addAttribute("boardsList", boardsList);
+			model.addAttribute("paging", paging);	
+		} else {
+			
+			List<MainDto> boardsList = boardsDao.findSearch(startNum, keyword);
+			PagingDto paging = boardsDao.paging(page, keyword);
+			paging.makeBlockInfo(keyword);
+			
+			model.addAttribute("boardsList", boardsList);
+			model.addAttribute("paging", paging);
+		}
+
 		return "boards/main";
+	
 	}
 
 	@GetMapping("/boards/{id}")
@@ -103,4 +174,3 @@ public class BoardsController {
 		return "boards/writeForm";
 	}
 }
-
---
+++ src/main/java/site/metacoding/red/web/dto/request/boards/UpdateDto.java
@@ -0,0 +1,14 @@
+package site.metacoding.red.web.dto.request.boards;
+
+import lombok.Getter;
+import lombok.Setter;
+import site.metacoding.red.domain.boards.Boards;
+
+@Setter
+@Getter
+public class UpdateDto {
+	private String title;
+	private String content;
+	
+
+}
